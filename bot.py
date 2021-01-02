b6adb598d0e247a3efffa0ef00a8123e7829660f
phangjunyu <phangjunyu@gmail.com>
2021-01-02 12:21:34 +0800

    changed main code

--- bot.py
+++ bot.py
@@ -20,38 +20,31 @@ timestamp = now.strftime("%Y-%m-%d %X")
 app = Flask(__name__)
 
 
-def main():
-    serve(app, host="0.0.0.0", port=int(PORT))
-    """Start the bot."""
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    try:
+        if request.method == "POST":
+            data = request.get_json()
+            key = data["key"]
+            if key == KEY:
+                print(timestamp, "Alert Received & Sent!")
+                tg_bot = Bot(token=TOKEN)
+                try:
+                    tg_bot.sendMessage(data["telegram"], data["msg"])
+                except KeyError:
+                    tg_bot.sendMessage(CHATID, data["msg"])
+                except Exception as e:
+                    print("[X] Telegram Error:\n>", e)
+                return "Sent alert", 200
 
-    @app.route("/webhook", methods=["POST"])
-    def webhook():
-        try:
-            if request.method == "POST":
-                data = request.get_json()
-                key = data["key"]
-                if key == KEY:
-                    print(timestamp, "Alert Received & Sent!")
-                    tg_bot = Bot(token=TOKEN)
-                    try:
-                        tg_bot.sendMessage(data["telegram"], data["msg"])
-                    except KeyError:
-                        tg_bot.sendMessage(CHATID, data["msg"])
-                    except Exception as e:
-                        print("[X] Telegram Error:\n>", e)
-                    return "Sent alert", 200
+            else:
+                print("[X]", timestamp, "Alert Received & Refused! (Wrong Key)")
+                return "Refused alert", 400
 
-                else:
-                    print("[X]", timestamp, "Alert Received & Refused! (Wrong Key)")
-                    return "Refused alert", 400
-
-        except Exception as e:
-            print("[X]", timestamp, "Error:\n>", e)
-            return "Error", 400
+    except Exception as e:
+        print("[X]", timestamp, "Error:\n>", e)
+        return "Error", 400
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+    serve(app, host="0.0.0.0", port=int(PORT))
