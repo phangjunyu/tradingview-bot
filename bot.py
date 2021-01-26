3dc5aaf95ff977bc46c3fc494c3b54368462e009
phangjunyu <phangjunyu@gmail.com>
2021-01-26 11:44:25 +0800

    binance order report

--- bot.py
+++ bot.py
@@ -9,16 +9,46 @@ import json
 from datetime import datetime
 
 from telegram import Bot
+from binance.client import Client
 
 PORT = int(os.environ.get("PORT", "8080"))
 
 TOKEN = os.environ.get("TOKEN")
 KEY = os.environ.get("KEY")
 CHATID = os.environ.get("CHATID")
+BINANCE_API_KEY = os.environ.get("BINANCE-API-KEY")
+BINANCE_SECRET_KEY = os.environ.get("BINANCE-SECRET-KEY")
 
 now = datetime.now()
 timestamp = now.strftime("%Y-%m-%d %X")
 app = Flask(__name__)
+
+
+def binance_report(client):
+    info = client.get_account()
+    update = ""
+    for b in info["balances"]:
+        _b = _readable(b)
+        update += _b + "/n/n"
+    return update
+
+
+def round_format(string, decimal):
+    return str(round(float(string), decimal))
+
+
+def _readable(b):
+    return (
+        "Available "
+        + b["asset"]
+        + ": "
+        + round_format(b["free"], 2)
+        + "/n"
+        + "Locked "
+        + b["asset"]
+        + ": "
+        + round_format(b["locked"], 2)
+    )
 
 
 @app.route("/webhook", methods=["POST", "GET"])
@@ -32,8 +62,10 @@ def webhook():
                 tg_bot = Bot(token=TOKEN)
                 try:
                     tg_bot.sendMessage(data["telegram"], data["msg"])
+                    tg_bot.sendMessage(data["telegram"], binance_report(client))
                 except KeyError:
                     tg_bot.sendMessage(CHATID, data["msg"])
+                    tg_bot.sendMessage(CHATID, binance_report(client))
                 except Exception as e:
                     print("[X] Telegram Error:\n>", e)
                 return "Sent alert", 200
@@ -51,3 +83,4 @@ def webhook():
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=int(PORT))
+    client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
